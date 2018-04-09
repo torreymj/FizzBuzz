07992837b88a5912a21f2885ae8710aee8bd4507
Micauly Torrey <36209828+torreymj@users.noreply.github.com>
2018-04-09 10:34:55 -0400

    Add files via upload

---
+++ fizzbuzztest.spec.js
@@ -0,0 +1,16 @@
+// fizzbuzztest.spec.js
+import the fizzbuzz.js library
+
+describe("A FizzBuzz program", () => {
+	
+	it("can count to 100", () => {
+	
+	expect(1), toBe(1);
+	expect(2), toBe(2);
+	expect(3), toBe('Fizz');
+	expect(4), toBe(4)
+	expect(5), toBe('Buzz');
+	expect(15), toBe('Fizzbuzz');
+
+  });
+});
